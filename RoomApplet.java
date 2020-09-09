9642d7f4e26b4834b5d1d2eebbebdd6e38e56621
mjgaughan <58380965+mjgaughan@users.noreply.github.com>
2020-09-08 17:14:20 -0700

    can now toggle between 4 options

--- RoomApplet.java
+++ RoomApplet.java
@@ -9,7 +9,11 @@ public class RoomApplet extends Applet implements ActionListener {
      */
     private static final long serialVersionUID = 1L;
 
+    protected int layout;
+
     public void init() {
+        setFont(new Font("Dialog", Font.BOLD, 18));
+
         layout1 = new Button("Layout 1");
         add(layout1);
         layout1.addActionListener(this);
@@ -25,23 +29,53 @@ public class RoomApplet extends Applet implements ActionListener {
         layout4 = new Button("Layout 4");
         add(layout4);
         layout4.addActionListener(this);
+
+        layout = 0;
+
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.orange);
-        g.fillRect(200, 100, 500, 500);
+        if (layout == 0) {
+            g.setColor(Color.orange);
+            g.fillRect(50, 50, 700, 100);
+        } else if (layout == 1) {
+            g.setColor(Color.green);
+            g.fillRect(50, 50, 700, 100);
+        } else if (layout == 2) {
+            g.setColor(Color.red);
+            g.fillRect(50, 50, 700, 100);
+        } else if (layout == 3) {
+            g.setColor(Color.blue);
+            g.fillRect(50, 50, 700, 100);
+        } else {
+            g.setColor(Color.black);
+            g.fillRect(50, 50, 700, 100);
+        }
+
     }
 
     Button layout1, layout2, layout3, layout4;
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == layout1)
+        if (e.getSource() == layout1) {
             System.out.println("Layout 1 chosen");
-        if (e.getSource() == layout2)
+            layout = 1;
+            repaint();
+        }
+        if (e.getSource() == layout2) {
             System.out.println("Layout 2 chosen");
-        if (e.getSource() == layout3)
+            layout = 2;
+            repaint();
+        }
+        if (e.getSource() == layout3) {
             System.out.println("Layout 3 chosen");
-        if (e.getSource() == layout4)
+            layout = 3;
+            repaint();
+        }
+        if (e.getSource() == layout4) {
             System.out.println("Layout 4 chosen");
+            layout = 4;
+            repaint();
+        }
     }
 }
